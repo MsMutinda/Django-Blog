ba83ee31a2b0b096d8bcbfcdc0a7ad966c9749c4
MsMutinda <MsMutinda@users.noreply.github.com>
2021-11-29 16:02:00 +0300

    another commit

--- myblog/models.py
+++ myblog/models.py
@@ -10,6 +10,9 @@ from django.utils import timezone
 class Category(models.Model):
     name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.name
+
 
 class Post(models.Model):
     category = models.ForeignKey(Category, default=1, on_delete=models.CASCADE)
