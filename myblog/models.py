3f5e1a5bed852a2f5ecfd7a104fab1488f99820e
MsMutinda <MsMutinda@users.noreply.github.com>
2021-12-10 12:31:42 +0300

    notification bar

--- myblog/models.py
+++ myblog/models.py
@@ -25,6 +25,7 @@ class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()  # for long texts without any character limit
+    likes = models.ManyToManyField(User, related_name='blog_posts')
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
 
